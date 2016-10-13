213c6ae6b6998a1648764d04fd81f12842e42598
paroperha <paroperha@users.noreply.github.com>
2016-10-13 22:42:31 +1100

    Fixed didn't find anything bug

--- details.py
+++ details.py
@@ -3,7 +3,9 @@ def details(f):
 
     file = f.readlines()
     for line in file:
-        if line[0][-1] == ":":
-            out.write(" ".join(line))
+        line = line.replace('"',"").replace("'","").split(",")
+        if len(line)>1:
+            if line[0] and line[0][-1] == ":" and line[1] and line[1] != "\n" and line[1] != "Not Entered\n":
+                    out.write(" ".join(line))
 
     out.close()
